e3e10355a25915cf98d67cf4a31d2eb806932e96
arashesfandiari <arashesfandiari@yahoo.com>
2023-10-18 19:02:29 -0400

    Revert "HashRouter"
    This reverts commit acd1bf59e3ccb68298a284f814a3c2b1dd057547.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { createBrowserHistory } from 'history';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import {createBrowserHistory} from 'history';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -51,55 +51,55 @@ const appHistory = {
 };
 const history = createBrowserHistory(appHistory);
 function App() {
-
+ 
   return (
     <div className="page">
-      <Router basename={process.env.PUBLIC_URL} history={history}>
+      <Router basename={process.env.PUBLIC_URL} history={history}>  
         <div id="preloader">
           <div id="status">&nbsp;</div>
-        </div>
+        </div>   
         <ScrollToTop />
-        <Route exact path='/' component={Home} />
-        <Route path='/home2' component={Home2} />
-        <Route path='/home3' component={Home3} />
-        <Route path='/header_style_02' component={Home2_Header2} />
-        <Route path='/header_style_03' component={Home3_Header3} />
-        <Route path="/aboutus_01" component={Aboutus_01} />
-        <Route path="/aboutus_02" component={Aboutus_02} />
-        <Route path="/services_01" component={Services_01} />
-        <Route path="/services_02" component={Services_02} />
-        <Route path="/our_expert" component={Our_expert} />
-        <Route path="/team_detail" component={Team_detail} />
-        <Route path="/faq" component={Faq} />
-        <Route path="/contact_01" component={Contact_01} />
-        <Route path="/contact_02" component={Contact_02} />
-        <Route path="/error" component={Error} />
-        <Route path="/element" component={Element} />
-        <Route path="/it_consultancy" component={It_consultancy} />
-        <Route path="/digital_services" component={Digital_services} />
-        <Route path="/advisory_services" component={Advisory_services} />
-        <Route path="/data_structuring" component={Data_structuring} />
-        <Route path="/experience_design" component={Experience_design} />
-        <Route path="/content_engineering" component={Content_engineering} />
-        <Route path="/project_style1" component={Project_style1} />
-        <Route path="/project_style2" component={Project_style2} />
-        <Route path="/single_style1" component={Single_style1} />
-        <Route path="/single_style2" component={Single_style2} />
-        <Route path="/single_style3" component={Single_style3} />
-        <Route path="/blog" component={Blog} />
-        <Route path="/blog_grid" component={Blog_grid} />
-        <Route path="/blog_left_image" component={Blog_left_image} />
-        <Route path="/blog_top_image" component={Blog_top_image} />
-        <Route path="/single_blog" component={Single_blog} />
-        <Route path="/shop" component={Shop} />
-        <Route path="/product_details" component={Product_details} />
-        <Route path="/cart" component={Cart} />
-        <Route path="/checkout" component={Checkout} />
+            <Route exact path='/' component={ Home } /> 
+            <Route path='/home2' component={ Home2 } /> 
+            <Route path='/home3' component={ Home3 } />
+            <Route path='/header_style_02' component={ Home2_Header2 } />
+            <Route path='/header_style_03' component={ Home3_Header3 } />
+            <Route path="/aboutus_01" component={Aboutus_01} />
+            <Route path="/aboutus_02" component={Aboutus_02} />
+            <Route path="/services_01" component={Services_01} />
+            <Route path="/services_02" component={Services_02} />
+            <Route path="/our_expert" component={Our_expert} />
+            <Route path="/team_detail" component={Team_detail} />
+            <Route path="/faq" component={Faq} />
+            <Route path="/contact_01" component={Contact_01} />
+            <Route path="/contact_02" component={Contact_02} />
+            <Route path="/error" component={Error} />
+            <Route path="/element" component={Element} />
+            <Route path="/it_consultancy" component={It_consultancy} />
+            <Route path="/digital_services" component={Digital_services} />
+            <Route path="/advisory_services" component={Advisory_services} />
+            <Route path="/data_structuring" component={Data_structuring} />
+            <Route path="/experience_design" component={Experience_design} />
+            <Route path="/content_engineering" component={Content_engineering} />
+            <Route path="/project_style1" component={Project_style1} />
+            <Route path="/project_style2" component={Project_style2} />
+            <Route path="/single_style1" component={Single_style1} />
+            <Route path="/single_style2" component={Single_style2} />
+            <Route path="/single_style3" component={Single_style3} />
+            <Route path="/blog" component={Blog} />
+            <Route path="/blog_grid" component={Blog_grid} />
+            <Route path="/blog_left_image" component={Blog_left_image} />
+            <Route path="/blog_top_image" component={Blog_top_image} />
+            <Route path="/single_blog" component={Single_blog} />
+            <Route path="/shop" component={Shop} />
+            <Route path="/product_details" component={Product_details} />
+            <Route path="/cart" component={Cart} />
+            <Route path="/checkout" component={Checkout} />
         <Footer />
-      </Router>
-    </div>
+    </Router>  
+    </div> 
   );
-}
+ }
 
 export default App;
 
