0751c31096b28eb42da4104e3b52444164fb3eef
Arash Esfandiari <arashesfandiari@yahoo.com>
2023-11-10 01:55:09 -0500

    fixed whatsapp

---
+++ staticfiles/precache-manifest.76dc1d43103227cfcc06f93da029c8c3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f463baa3c3df49368938eb55a35d53c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35e7f1c4934cfeb97de9",
+    "url": "/static/css/2.bb2ce5cb.chunk.css"
+  },
+  {
+    "revision": "cd0ffd6465d41ebb522b",
+    "url": "/static/css/main.4c48667a.chunk.css"
+  },
+  {
+    "revision": "35e7f1c4934cfeb97de9",
+    "url": "/static/js/2.b43dfbdf.chunk.js"
+  },
+  {
+    "revision": "dfd79e58f10ff0598355385ce078bb7f",
+    "url": "/static/js/2.b43dfbdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd0ffd6465d41ebb522b",
+    "url": "/static/js/main.a972a604.chunk.js"
+  },
+  {
+    "revision": "8e311dc835941408b515",
+    "url": "/static/js/runtime-main.4b100dee.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
